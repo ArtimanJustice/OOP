3e5314add94a1acc61c52b8228df92b2931387e5
DanyloPetrov <danylo.p.android.dev@wildwildgroup.com>
2026-06-21 15:38:33 +0300

    Add explicit program entry point

---
+++ lab3lab4/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ShadowMaiden;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
